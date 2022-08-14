a68db03a748f1c59e04da46159d95258e469f891
Luis Soto <alberto.soto0409@gmail.com>
2022-08-14 18:21:02 -0500

    creación ProductoRepository

---
+++ src/main/java/com/soto/marketspring/persistence/crud/ProductoCrudRepository.java
@@ -0,0 +1,16 @@
+package com.soto.marketspring.persistence.crud;
+
+import com.soto.marketspring.persistence.entity.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductoCrudRepository extends CrudRepository<Producto,Integer> {
+
+      List<Producto> findByCategoriaIdOrderByNombreProductoAsc(int categoriaId);
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidad_stock, boolean estado);
+}
