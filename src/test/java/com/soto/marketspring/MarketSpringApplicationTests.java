f807b5994190d601607f1b26c89de9f1e11f4848
Luis Soto <alberto.soto0409@gmail.com>
2022-08-14 13:48:54 -0500

    resources

---
+++ src/test/java/com/soto/marketspring/MarketSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soto.marketspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
