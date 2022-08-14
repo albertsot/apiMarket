f2bf9a3ee2c15a6df6909cc6e923e4102bcb358c
Luis Soto <alberto.soto0409@gmail.com>
2022-08-14 18:21:02 -0500

    creación RepositoryProductos

---
+++ src/main/java/com/soto/marketspring/persistence/crud/ProductoRepository.java
@@ -0,0 +1,36 @@
+package com.soto.marketspring.persistence.crud;
+
+import com.soto.marketspring.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class ProductoRepository {
+
+    private ProductoCrudRepository productoCrudRepository;
+
+    public List<Producto> getAll(){
+        return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    public List<Producto> getByCategoria(int categoriaId){
+        return productoCrudRepository.findByCategoriaIdOrderByNombreProductoAsc(categoriaId);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad,true);
+    }
+
+    public Optional<Producto> getProducto(int productoId){
+        return productoCrudRepository.findById(productoId);
+    }
+
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+    public void delete(int productoId){
+        productoCrudRepository.deleteById(productoId);
+    }
+}
