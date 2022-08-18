3ac460d30811c8479a3cdb9a7eafd541bc8b032e
Luis Soto <alberto.soto0409@gmail.com>
2022-08-18 13:42:53 -0500

    exposición de APIS con Swagger2

--- src/main/java/com/soto/marketspring/web/config/SwaggerConfig.java
+++ src/main/java/com/soto/marketspring/web/config/SwaggerConfig.java
@@ -1,2 +1,30 @@
-package com.soto.marketspring.web.config;public class SwaggerConfig {
+package com.soto.marketspring.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.soto.marketspring.web.controllers"))
+                .build().apiInfo(apiEndPointInfo());
+    }
+
+    private ApiInfo apiEndPointInfo() {
+        return new ApiInfoBuilder().title("API-MARKET")
+                .description("Servicios para la consulta de productos y compras de un supermercado")
+                .license("Apache 2.0")
+                .version("1.0.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .build();
+    }
 }
